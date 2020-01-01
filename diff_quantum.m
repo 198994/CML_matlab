0509376b17f2a3a823cb385332b6a92ca13bea66
198994 <230198994@seu.edu.cn>
2020-01-01 20:04:02 +0800

    增加一篇文章

---
+++ diff_quantum.m
@@ -0,0 +1,11 @@
+%   Á¿×ÓÖÃÂÒËã·¨
+% P4 = imread(P4);
+[m n] = size(P4);
+for i =1:m 
+    for j = 1:n 
+%         bin_P4 = dec2binvec(P4(m,n),8);
+    end
+end
+
+
+% kron(value_color,value_position)
